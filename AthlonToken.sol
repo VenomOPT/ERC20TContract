78711880cde3c776fae10e975493bd4b1b48e89e
Marin Constantin Horobet <111706680+VenomOPT@users.noreply.github.com>
2022-09-03 19:45:57 +0200

    Update AthlonToken.sol

--- AthlonToken.sol
+++ AthlonToken.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.16;
 
 // ----------------------------------------------------------------------------
 // ERC Token Standard #20 Interface
 //
 // ----------------------------------------------------------------------------
+
 contract ERC20Interface {
     function totalSupply() public view returns (uint);
     function balanceOf(address tokenOwner) public view returns (uint balance);
@@ -12,16 +14,19 @@ contract ERC20Interface {
     function transfer(address to, uint tokens) public returns (bool success);
     function approve(address spender, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
-    function mint(address to, uint amount) external);
-    function burn (uint amount) external);
-
+    function _mint(address to, uint tokenId) internal {    require(to != address(0));    require(!_exists(tokenId));    _tokenOwner[tokenId] = to;    _ownedTokensCount[to] = _ownedTokensCount[to].add(1);    emit Transfer(address(0), to, tokenId);}
+    
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
+
 // ----------------------------------------------------------------------------
 // Safe Math Library
 // ----------------------------------------------------------------------------
+/**
+ * Math operations with safety checks
+ */
 contract SafeMath {
     function safeAdd(uint a, uint b) public pure returns (uint c) {
         c = a + b;
@@ -31,10 +36,28 @@ contract SafeMath {
         require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0);
         c = a / b;
     }
+
+    function safeDiv(uint256 a, uint256 b) internal returns (uint256) {
+    assert(b > 0);
+    uint256 c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+    function safeMul(uint256 a, uint256 b) internal returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+    function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
+    }
 }
 
 
-contract CodeWithJoe is ERC20Interface, SafeMath {
+contract Athlon is ERC20Interface, SafeMath {
     string public name;
     string public symbol;
     uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
@@ -59,6 +82,23 @@ contract CodeWithJoe is ERC20Interface, SafeMath {
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
+     /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        if (_to == 0x0) throw;                               // Prevent transfer to 0x0 address. Use burn() instead
+		if (_value <= 0) throw; 
+        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
+        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                     // Subtract from the sender
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to,amount);
+    }
+
+
     function totalSupply() public view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
@@ -77,12 +117,21 @@ contract CodeWithJoe is ERC20Interface, SafeMath {
         return true;
     }
 
-    function mint(address to, uint amount) external {
-        require(msg.sender == admin, 'only admin');
-        _mint(to,amount);
+    function freeze(uint256 _value) returns (bool success) {
+        if (balanceOf[msg.sender] < _value) throw;            // Check if the sender has enough
+		if (_value <= 0) throw; 
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                      // Subtract from the sender
+        freezeOf[msg.sender] = SafeMath.safeAdd(freezeOf[msg.sender], _value);                                // Updates totalSupply
+        Freeze(msg.sender, _value);
+        return true;
     }
 
-    function burn (uint amount) external {
-        _burn(msg.sender, amount);
+    function unfreeze(uint256 _value) returns (bool success) {
+        if (freezeOf[msg.sender] < _value) throw;            // Check if the sender has enough
+		if (_value <= 0) throw; 
+        freezeOf[msg.sender] = SafeMath.safeSub(freezeOf[msg.sender], _value);                      // Subtract from the sender
+		balanceOf[msg.sender] = SafeMath.safeAdd(balanceOf[msg.sender], _value);
+        Unfreeze(msg.sender, _value);
+        return true;
     }
-}          
+}
